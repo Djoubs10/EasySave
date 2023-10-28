9cadbacce9b64a6c66f39e905eac74948c7e20f5
Nassim Djoubri <nassim.djoubri@viacesi.fr>
2023-10-29 00:25:09 +0200

    Add Transfers view and viewmodel

--- ViewModels/TransfersViewModel.cs
+++ ViewModels/TransfersViewModel.cs
@@ -1,6 +1,9 @@
-﻿using EasySave.Models;
+﻿using EasySave.Helpers;
+using EasySave.Models;
 using System.CodeDom;
 using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows.Input;
 
 namespace EasySave.ViewModels
 {
@@ -8,9 +11,25 @@ namespace EasySave.ViewModels
     {
         private ObservableCollection<Transfer> _transfers;
         public ObservableCollection<Transfer> Transfers { get => _transfers; set {  _transfers = value; OnPropertyChanged(); } }
+        public ICommand StartTransferCommand { get; }
+        private void StartTransfer(object? parameter)
+        {
+            if(parameter is Transfer transfer)
+            {
+                Thread t = new Thread(transfer.Start);
+                t.Start();
+            }
+        }
+
         public TransfersViewModel()
         {
-            _transfers = new ObservableCollection<Transfer>();
+            _transfers = new ObservableCollection<Transfer>()
+            {
+                new Transfer("Transfer 1"),
+                new Transfer("Transfer 2")
+            };
+            StartTransferCommand = new RelayCommand(StartTransfer);
         }
+
     }
 }
