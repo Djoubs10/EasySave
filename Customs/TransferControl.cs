7ed9e06b4279a0a86c7fbf5aca1d6cbf1a5c526a
Nassim Djoubri <nassim.djoubri@viacesi.fr>
2023-10-30 18:18:22 +0100

    Add files ?

---
+++ Customs/TransferControl.cs
@@ -0,0 +1,33 @@
+﻿using EasySave.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EasySave.Customs
+{
+    public class TransferControl : Control
+    {
+        public ICommand StartCommand
+        {
+            get { return (ICommand)GetValue(StartCommandProperty); }
+            set { SetValue(StartCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StartCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartCommandProperty =
+            DependencyProperty.Register("StartCommand", typeof(ICommand), typeof(TransferControl), new PropertyMetadata(null));
+
+        public Transfer Transfer
+        {
+            get { return (Transfer)GetValue(TransferProperty); }
+            set { SetValue(TransferProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TransferProperty =
+            DependencyProperty.Register("Transfer", typeof(Transfer), typeof(TransferControl), new PropertyMetadata(null));
+        static TransferControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TransferControl), new FrameworkPropertyMetadata(typeof(TransferControl)));
+        }
+    }
+}
