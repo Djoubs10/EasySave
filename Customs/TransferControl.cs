7a1c8e0ee976931490c18553ac7026790d0b3e67
Nassim DJOUBRI <ndjoubri@clinique-sainthilaire.fr>
2023-10-30 09:27:13 +0100

    Creating custom control for transfers

---
+++ Customs/TransferControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using EasySave.Models;
+namespace EasySave.Customs
+{
+ 
+    public class TransferControl : Control
+    {
+
+
+
+
+        public Transfer Transfer
+        {
+            get { return (Transfer)GetValue(TransferProperty); }
+            set { SetValue(TransferProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Transfer.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TransferProperty =
+            DependencyProperty.Register("Transfer", typeof(Transfer), typeof(TransferControl), new PropertyMetadata(null));
+
+
+        static TransferControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TransferControl), new FrameworkPropertyMetadata(typeof(TransferControl)));
+        }
+    }
+}
